747e5afdce640f6df185eb4f7af9ddc2a9332a32
Azeem Khan <121457811+AzeemKhan27@users.noreply.github.com>
2024-06-16 11:50:01 +0000

    Basic : Variables and JS Code

---
+++ 01_basics/1_variable.js
@@ -0,0 +1,16 @@
+const accId = 12456;
+let accEmail = "azeem@google.com";
+var accPassword = "81258";
+accCity = "Delhi";
+let defaultValueInJS;
+
+// accId = 2 //Not Allowed
+
+console.log(accId);
+
+accEmail = "ak@google.com";
+accPassword = "211123";
+accCity = "Bangaluru";
+
+console.table([accId,accEmail,accPassword,accCity,defaultValueInJS]);
+
