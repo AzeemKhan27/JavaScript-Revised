2c9f25f557c38ccfc962ea34e428f92c947463cf
Azeem Khan <121457811+AzeemKhan27@users.noreply.github.com>
2024-06-17 08:45:00 +0000

    Revised Code : JavaScript

---
+++ 01_basics/3_conversion_operations.js
@@ -0,0 +1,15 @@
+
+let checkValues =  "azeem"; //true; //"12ABC"; //undefined; // null; 
+
+
+console.log("=======CHECK_VALUE======>");
+
+let convertInNum = Number(checkValues);
+console.log("datatype of original value : ",typeof checkValues);
+console.log("dataType of convertedValueInNumber : ",typeof convertInNum);
+
+console.log("Actual Value :", checkValues);
+console.log("Converted Value:",convertInNum);
+
+
+
